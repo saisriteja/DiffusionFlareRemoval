cb3120d25f8ebb0d4bde44831c2611604c62d76f
saisriteja <kuppasaisriteja@gmail.com>
2024-03-10 16:29:32 +0530

    data preparation

---
+++ SyntheticVideoGeneration/req.sh
@@ -0,0 +1,9 @@
+# pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1
+pip install napm einops omegaconf safetensors diffusers[torch]==0.11.1 transformers
+pip install pandas
+pip install decord
+# pip install xformers
+apt -y install -qq aria2 
+
+pip install gdown
+gdown 1PPXWxn7gYvqwHX301SuWmjI7IUUtqxab
